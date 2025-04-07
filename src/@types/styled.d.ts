8c69297156009746a6f956547b9c3388d0efe151
Ariovaldo Moraes <88855698+AriovaldoJM@users.noreply.github.com>
2025-04-07 08:35:58 -0300

    Update 07_04

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,7 @@
+import 'style-components/native';
+import theme from '';
+
+declare module 'styled-components' {
+    type ThemeType = typeof theme;
+    export interface DefaultTheme extends ThemeType {}  
+}
